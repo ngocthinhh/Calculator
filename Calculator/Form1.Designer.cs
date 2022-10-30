950bf1bc9452dcd2eb4b600f615f99ea904d93af
ngocthinhh <ngcthnh2@gmail.com>
2022-10-30 22:09:09 +0700

    Add cmt

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -3,7 +3,7 @@ namespace Calculator
 {
     partial class Form1
     {
-        /// <summary> 
+        /// <summary> asd
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
