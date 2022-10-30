2f9255bfe2dc4d13c7a8685224c1252748e1409c
ngocthinhh <ngcthnh2@gmail.com>
2022-10-30 22:11:51 +0700

    delete cmt

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -3,7 +3,7 @@ namespace Calculator
 {
     partial class Form1
     {
-        /// <summary> asd
+        /// <summary>
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
