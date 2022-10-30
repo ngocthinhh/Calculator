5f5f726244853d44114faf25a7516f2cc12fe0ef
ngocthinhh <ngcthnh2@gmail.com>
2022-10-30 21:40:18 +0700

    Add Button

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace Calculator
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -49,11 +50,21 @@ namespace Calculator
             this.textBox2.Size = new System.Drawing.Size(270, 26);
             this.textBox2.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -67,6 +78,7 @@ namespace Calculator
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
