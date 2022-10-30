b08613c8b1e743b03fa905b94d67038fda9a118e
ngocthinhh <ngcthnh2@gmail.com>
2022-10-30 22:46:32 +0700

    Update

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -3,7 +3,7 @@ namespace Calculator
 {
     partial class Form1
     {
-        /// <summary> eee
+        /// <summary> xxx
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
